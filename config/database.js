a6f092db6b7c8c0ce579a21d41b18a173af79ba7
LemuelFigueira <lemuelfigueira@unigranrio.br>
2020-09-16 12:24:21 -0300

    mudança

--- config/database.js
+++ config/database.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+var password = 'Qh10fz7O3CAkq0vQ';
+var dbname = 'to-do-list';
+var MONGO_URL = `mongodb+srv://lemuel:${password}@lemueldev.tlqp1.gcp.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
 mongoose.connect('mongodb://localhost/todo-list', {
     useNewUrlParser: true,
     useUnifiedTopology: true
